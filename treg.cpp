ba0e35aee808940844701b4f762f11cfef111395
Abarenov Egor <somgnom00@gmail.com>
2026-03-12 05:55:53 +0000

    добавление в код проверку на сущ треугольника

--- treg.cpp
+++ treg.cpp
@@ -3,23 +3,28 @@
 
 int main() {
     double a, b, c;
-    std::cout << "Введите стороны треугольника: ";
+
+    std::cout << "Введите три стороны треугольника (через пробел): ";
     std::cin >> a >> b >> c;
 
-    // 1. Периметр
-    double p = a + b + c;
-    std::cout << "Периметр: " << p << std::endl;
+    // ПРОВЕРКА
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        std::cout << "Треугольник с такими сторонами не существует" << std::endl;
+        return 1; 
+    }
 
-    // 2. Площадь по формуле Герона
-    double s_p = p / 2; // полупериметр
-    double area = sqrt(s_p * (s_p - a) * (s_p - b) * (s_p - c));
-    std::cout << "Площадь: " << area << std::endl;
+    // если все чикипуки, считаем дальше
+    double P = a + b + c;
+    double p = P / 2;
+    double S = sqrt(p * (p - a) * (p - b) * (p - c));
 
-    // 3. Проверка на равнобедренность
+    std::cout << "Периметр: " << P << std::endl;
+    std::cout << "Площадь: " << S << std::endl;
+
     if (a == b || b == c || a == c) {
-        std::cout << "Треугольник равнобедренный" << std::endl;
+        std::cout << "Треугольник равнобедренный." << std::endl;
     } else {
-        std::cout << "Треугольник не равнобедренный" << std::endl;
+        std::cout << "Треугольник не равнобедренный." << std::endl;
     }
 
     return 0;
